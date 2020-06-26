9721b6da098f34257ecff53290326f1ddfa367e6
Saman Rashidi <samanrashidi44@gmail.com>
2020-06-26 17:17:00 +0430

    feature: added howToPlay in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
-    <Logo />
-    <Title
-      :value="$t('home.mainTitle')"
-      size="xlg"
-    />
     <div class="frame">
+      <Logo />
+      <Title
+        :value="$t('home.mainTitle')"
+        size="xlg"
+        class="mt-2"
+      />
       <div
         v-if="profileBoxes"
         class="profile-box-wrapper"
@@ -16,6 +17,17 @@
           :image="profile.image"
           :name="profile.name"
           @click.native="profileClicked(profile)"
+        />
+      </div>
+      <Title
+        :value="$t('howToPlay.title')"
+        size="lg"
+        class="mt-3 pt-3"
+      />
+      <div class="frame frame-sm">
+        <p
+          v-html="$t('howToPlay.description')"
+          class="mt-2 text-center"
         />
       </div>
     </div>
