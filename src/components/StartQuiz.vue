2ac1fee6d6781b50e0ca1503e9c21990a79ed978
Saman Rashidi <samanrashidi44@gmail.com>
2020-06-26 19:50:49 +0430

    feature: startQuiz step finished in quiz page

--- src/components/StartQuiz.vue
+++ src/components/StartQuiz.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="start-quiz step-1">
-    <template v-if="profile">
-      <ProfileBox
-        :image="profile.image"
-        :name="profile.name"
+    <ProfileBox
+      :image="profile.image"
+      :name="profile.name"
+    />
+    <form
+      class="mt-3"
+      @submit.prevent="usernameInserted()"
+    >
+      <input
+        v-model="username"
+        type="text"
+        name="username"
+        :placeholder="$t('quiz.usernamePlaceholder')"
       />
-    </template>
+      <button type="submit">
+        {{ $t('quiz.startQuizButton') }}
+      </button>
+    </form>
   </div>
 </template>
 
@@ -20,6 +32,16 @@ export default {
     profile: {
       type: Object,
       default: () => {}
+    }
+  },
+  data () {
+    return {
+      username: null
+    }
+  },
+  methods: {
+    usernameInserted () {
+      alert(this.username)
     }
   }
 }
