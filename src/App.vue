58c0a6145cb57801458463ec7ff45a2971a332a1
Saman Rashidi <samanrashidi44@gmail.com>
2020-06-26 17:49:34 +0430

    feature: social component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <template>
   <div id="app">
     <router-view/>
+    <Socials />
   </div>
 </template>
+
+<script>
+import Socials from '@/components/Socials.vue'
+export default {
+  components: {
+    Socials
+  }
+}
+</script>
 
 <style lang="scss">
 @import "./sass/main";
---
+++ src/components/Socials.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="socials">
+    <ul>
+        <li
+            v-for="social in socials"
+            :key="social.id"
+        >
+            <a
+                :href="social.url"
+                target="_blank"
+                :title="social.name"
+            >
+                <img
+                    :src="require(`@/assets/socials/${social.icon}`)"
+                    :alt="social.name"
+                >
+            </a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      socials: [
+        {
+          id: 0,
+          name: 'Github',
+          icon: 'github.svg',
+          url: 'https://github.com/samanrashidii/youdonome'
+        },
+        {
+          id: 1,
+          name: 'Instagram',
+          icon: 'instagram.svg',
+          url: 'https://www.instagram.com/mad.vortex'
+        }
+      ]
+    }
+  }
+}
+</script>
